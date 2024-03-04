482cc649684ca22d53a8237db18afe7be0a3988a
kourosh karimi <karimikorosh2000@gmail.com>
2024-03-04 20:24:54 +0330

    Villa CRUD and Images

--- VillaWeb/Controllers/VillaController.cs
+++ VillaWeb/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Villa.Infrastructure.Data;
 
@@ -33,6 +34,57 @@ public class VillaController : Controller
             _db.SaveChanges();
 
             return RedirectToAction(nameof(Index));
+        }
+
+        return View();
+    }
+
+    public IActionResult Update(int villaId)
+    {
+        Villa.Domain.Entities.Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+        if (obj == null)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+
+        return View(obj);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Villa.Domain.Entities.Villa obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Villas.Update(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View();
+    }
+
+    public IActionResult Delete(int villaId)
+    {
+        Villa.Domain.Entities.Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+        if (obj == null)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+
+        return View(obj);
+    }
+
+
+    [HttpPost]
+    public IActionResult Delete(Villa.Domain.Entities.Villa obj)
+    {
+        Villa.Domain.Entities.Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+        if (objFromDb is not null)
+        {
+            _db.Villas.Remove(objFromDb);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         return View();
